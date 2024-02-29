2a9b0d12e438395dac0d9bb09c9caeaa0dcae9de
Kevin Hogan <kchogan@pacbell.net>
2024-02-28 20:54:34 -0800

    fixed formatting issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import './reset.css'
 import './App.css'
+import image01 from './assets/images/image-01.jpg';
+import image02 from './assets/images/image-02.jpg';
+import image03 from './assets/images/image-03.jpg';
+import image04 from './assets/images/image-04.jpg';
+import image05 from './assets/images/image-05.jpg';
+import image06 from './assets/images/image-06.jpg';
+import portrait from './assets/images/portrait.jpg';
 
 function App() {
 
@@ -30,7 +37,7 @@ function App() {
         <section className="about-me">
           <h2 id="About_Me">About Me</h2>
           <div>
-            <img className="portrait" src="./assets/images/portrait.jpg" />
+            <img className="portrait" src={portrait} />
             <p>
               I have spent over 30 years in the video game industry as a producer,
               designer, and development director, having worked on some of the
@@ -51,7 +58,7 @@ function App() {
             <a target="_blank" href="https://kevinchogan.github.io/module-01/">
               <figure className="card">
                 <h3 className="card-header">Web Marketing Services</h3>
-                <img className="card-image" src="./assets/images/image-06.jpg" />
+                <img className="card-image" src={image06} />
                 <div className="card-body">
                   Refactored existing code in a web application to include
                   accessibility features, improve code readability, and adhere to
@@ -66,7 +73,7 @@ function App() {
             <a target="_blank" href="#">
               <figure className="card">
                 <h3 className="card-header">Lorem Ipsum</h3>
-                <img className="card-image" src="./assets/images/image-02.jpg" />
+                <img className="card-image" src={image02} />
                 <div className="card-body">
                   Lorem ipsum dolor sit amet, consectetur adipisci elit, sed
                   eiusmod tempor incidunt ut labore et dolore magna aliqua.
@@ -78,7 +85,7 @@ function App() {
             <a target="_blank" href="#">
               <figure className="card">
                 <h3 className="card-header">Dolor Sit</h3>
-                <img className="card-image" src="./assets/images/image-01.jpg" />
+                <img className="card-image" src={image01} />
                 <div className="card-body">
                   Lorem ipsum dolor sit amet, consectetur adipisci elit, sed
                   eiusmod tempor incidunt ut labore et dolore magna aliqua.
@@ -90,7 +97,7 @@ function App() {
             <a target="_blank" href="#">
               <figure className="card">
                 <h3 className="card-header">Consectetur Adipisci Elit</h3>
-                <img className="card-image" src="./assets/images/image-04.jpg" />
+                <img className="card-image" src={image04} />
                 <div className="card-body">
                   Lorem ipsum dolor sit amet, consectetur adipisci elit, sed
                   eiusmod tempor incidunt ut labore et dolore magna aliqua.
@@ -102,7 +109,7 @@ function App() {
             <a target="_blank" href="#">
               <figure className="card">
                 <h3 className="card-header">Corporis Suscipit</h3>
-                <img className="card-image" src="./assets/images/image-05.jpg" />
+                <img className="card-image" src={image05} />
                 <div className="card-body">
                   Lorem ipsum dolor sit amet, consectetur adipisci elit, sed
                   eiusmod tempor incidunt ut labore et dolore magna aliqua.
@@ -114,7 +121,7 @@ function App() {
             <a target="_blank" href="#">
               <figure className="card">
                 <h3 className="card-header">Nulla Pariatur</h3>
-                <img className="card-image" src="./assets/images/image-03.jpg" />
+                <img className="card-image" src={image03} />
                 <div className="card-body">
                   Lorem ipsum dolor sit amet, consectetur adipisci elit, sed
                   eiusmod tempor incidunt ut labore et dolore magna aliqua.
