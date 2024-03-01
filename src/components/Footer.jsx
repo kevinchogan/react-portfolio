e69abfaad356d4746525732a84226beeff74026b
Kevin Hogan <kchogan@pacbell.net>
2024-03-01 07:54:50 -0800

    finished footer icon links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
+import github from '../assets/icons/github.png'
+import linkedin from '../assets/icons/linkedin.png'
+import facebook from '../assets/icons/facebook.png'
+
 export default function Footer() {
     return (
         <footer>
-        <h3>Made with <span role="img" aria-label="heart">❤️</span> and CSS</h3>
+        <a href="https://www.linkedin.com/in/kevin-hogan-aa53392/" target="_blank">
+          {/* <img id='linkedin' src={linkedin}></img> */}
+          <div id='linkedin'></div>
+        </a>
+        <a href="https://github.com/kevinchogan" target="_blank">
+          {/* <img id='github' src={github}></img> */}
+          <div id='github'></div>
+        </a>
+        <a href="https://www.facebook.com/kevin.hogan.378" target="_blank">
+          {/* <img id='facebook' src={facebook}></img> */}
+          <div id='facebook'></div>
+        </a>
       </footer>
     );
   }
