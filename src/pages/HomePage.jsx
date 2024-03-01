b940aeb43d5ea3f6877ec7725e4f1051fd0cdde2
Kevin Hogan <kchogan@pacbell.net>
2024-03-01 06:26:22 -0800

    about page edits and formatting

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,9 @@ export default function AboutPage() {
     return (
         <section className="about-me">
           <h2 id="About_Me">About Me</h2>
-          <div>
+          <div className="about-container">
             <img className="portrait" src={portrait} />
-            <p>
+            <p className="portrait-body">
               I have spent over 30 years in the video game industry as a producer,
               designer, and development director, having worked on some of the
               most successful franchises in the industry&#39;s history including
@@ -16,6 +16,12 @@ export default function AboutPage() {
               video gaming I taught myself VBA. I now intend to spend the second
               half of my life focused on pursuing that passion full-time as a web
               application developer.
+            </p>
+            <p className="portrait-body">
+              To that end, I have immersed myself in the web development world working
+              with javascript, node.js, jquery, JSON, Express, MySQL, Sequelize, PWA,
+              MongoDB, Mongoose, React, and MERN to develop a number of projects viewable 
+              on the Portfolio tab of this web site.
             </p>
           </div>
         </section>
