0aa041e1fd92997074285451a8a17ce68321092d
Kevin Hogan <kchogan@pacbell.net>
2024-03-01 06:47:27 -0800

    first pass resume page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,11 +1,31 @@
+import resume from '../assets/pdf/resume.pdf'
+
 export default function ResumePage() {
     return (
         <section className="about-me">
           <h2 id="Resume">Resume</h2>
+          <p><a href={resume}>Download Resume</a></p>
           <div>
-            <p>
-                Resume goes here!
-            </p>
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>JQuery</li>
+              <li>Responsive Design</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+            </ul><br></br>
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+              <li>APIs</li>
+              <li>Node</li>
+              <li>Express</li>
+              <li>MySQL</li>
+              <li>Sequelize</li>
+              <li>MongoDB</li>
+              <li>Mongoose</li>
+            </ul>
           </div>
         </section>
     );
