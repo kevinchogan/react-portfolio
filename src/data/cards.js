f131b100f48fd719f60a3bf1393aafa2aa5fbe29
Kevin Hogan <kchogan@pacbell.net>
2024-03-02 22:11:57 -0800

    added real data to projects

--- src/data/cards.js
+++ src/data/cards.js
@@ -1,67 +1,180 @@
 import csscImage from '../assets/images/cssc.png'
-import image02 from '../assets/images/image-02.jpg'
-import image03 from '../assets/images/image-03.jpg'
-import image04 from '../assets/images/image-04.jpg'
-import image05 from '../assets/images/image-05.jpg'
-import image06 from '../assets/images/image-06.jpg'
-
+import logo from '../assets/images/logo.png'
+import ecommerce from '../assets/images/ecommerce.png'
+import emptracker from '../assets/images/emptracker.png'
+import weather from '../assets/images/weather.png'
+import beer from '../assets/images/beer.png'
+import socialnetwork from '../assets/images/socialnetwork.png'
+import quiz from '../assets/images/quiz.png'
+import readme from '../assets/images/readme.png'
 
 const Projects = () => {
     const projectList = [
     {
         "id": 1,
         "title": "Club Sandwich Sandwich Club",
-        "description": "An easy-to-use website for viewing, rating, and adding your favorite sandwich recipes",
+        "description": "An easy-to-use website for viewing, rating, and adding your favorite sandwich recipes.  The application includes encrypted user authentication, a SQL database for storing sandwich recipes, user information, and ratings, and full CRUD API implementation for all stored data.",
         "link": "https://club-sandwich-sandwich-club-ddb63265b5d8.herokuapp.com/",
+        "gitHub": "https://github.com/philsloan/Club-Sandwich-Sandwich-Club",
+        "tech": [
+            "html/CSS",
+            "Node.js",
+            "dotenv",
+            "bcrypt",
+            "express",
+            "express-session",
+            "MySQL",
+            "sequelize",
+            "handlebars",
+        ],
+        "video": "",
         "image": csscImage,
         "cardClass": "card-column",
-        "altText": "altTextTBD"
+        "altText": "Image of the club sandwich sandwich club website"
     },
     {
         "id": 2,
-        "title": "Lorem Ipsum",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
-        "link": "#",
-        "image": image02,
+        "title": "Logo Maker",
+        "description": "A Node.js command-line application that prompts the user to select a color, shape, and text to create a logo and save it as an SVG file. Includes a suite of auto-tests utilizing Jest.",
+        "link": "",
+        "gitHub": "https://github.com/kevinchogan/logo-maker",
+        "tech": [
+            "Node.js",
+            "Inquirer",
+            "Jest",
+        ],
+        "video": "https://youtu.be/lCa20vam9GE",
+        "image": logo,
         "cardClass": "card-column rest",
-        "altText": "altTextTBD"
+        "altText": "Image of the logo maker application running in a VS Code terminal"
     },
     {
         "id": 3,
-        "title": "Dolor Sit",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
-        "link": "#",
-        "image": image03,
+        "title": "E-commerce Backend",
+        "description": "The back end for an e-commerce site using Express.js API and Sequelize to interact with a MySQL database.",
+        "link": "https://ecommerce-backend-kevin-0fba4610ffb4.herokuapp.com/api/products",
+        "gitHub": "https://github.com/kevinchogan/e-commerce-backend",
+        "tech": [
+            "Node.js",
+            "Dotenv",
+            "Express",
+            "MySQL",
+            "Sequelize",
+            "Insomnia",
+        ],
+        "video": "https://www.youtube.com/watch?v=q-3N93Op4s0",
+        "image": ecommerce,
         "cardClass": "card-column rest",
-        "altText": "altTextTBD"
+        "altText": "Image of the e-commerce backend API calls being made in Insomnia"
     },
     {
         "id": 4,
-        "title": "Consectetur Adipisci Elit",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
-        "link": "#",
-        "image": image04,
+        "title": "Employee Tracker",
+        "description": "A command-line application to manage a company's employee database.",
+        "link": "",
+        "gitHub": "https://github.com/kevinchogan/employee-tracker",
+        "tech": [
+            "Node.js",
+            "Dotenv",
+            "Inquirer",
+            "Express",
+            "MySQL",
+        ],
+        "video": "https://youtu.be/CPEJyDaeMLE",        
+        "image": emptracker,
         "cardClass": "card-column rest",
-        "altText": "altTextTBD"
+        "altText": "Image of the employee tracker application running in a VS Code terminal"
     },
     {
         "id": 5,
-        "title": "Corporis Suscipit",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
-        "link": "#",
-        "image": image05,
+        "title": "Weather Dashboard",
+        "description": "A weather dashboard for showing the 5 day weather forecast for the city of your choice. Utilizes JQuery & JQuery UI to prompt the user to enter a city and fetch data from openweather.com to display weather for that city.",
+        "link": "https://kevinchogan.github.io/weather-dashboard/",
+        "gitHub": "https://github.com/kevinchogan/weather-dashboard",
+        "tech": [
+            "HTML/CSS",
+            "Javascript",
+            "JQuery",
+            "JQuery UI",
+            "Bootstrap",
+            "Moment.js",
+            "Open Weathermap API",
+        ],
+        "video": "",          
+        "image": weather,
         "cardClass": "card-column rest",
-        "altText": "altTextTBD"
+        "altText": "Image of the weather dashboard web site"
     },
     {
         "id": 6,
-        "title": "Nulla Pariatur",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
-        "link": "#",
-        "image": image06,
+        "title": "Beer Is Poetic",
+        "description": "This website allows you to search a city for a list of breweries and will generate a satellite image of the city as well as a random beer-related poem.",
+        "link": "https://jonsantosdev.github.io/beer-is-poetic/",
+        "gitHub": "https://github.com/jonsantosdev/beer-is-poetic",
+        "tech": [
+            "HTML/CSS",
+            "Javascript",
+            "JQuery",
+            "JQuery UI",
+            "Tailwinds",
+            "Open Brewery DB API",
+            "NASA Planetary Earth API",
+            "PoetryDB.org API"
+        ],
+        "video": "",          
+        "image": beer,
         "cardClass": "card-column rest",
-        "altText": "altTextTBD"
-    }
+        "altText": "Image of the beer and poetry web site"
+    },
+    {
+        "id": 7,
+        "title": "Social Network API",
+        "description": "An API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.",
+        "link": "",
+        "gitHub": "https://github.com/kevinchogan/social-network-api",
+        "tech": [
+            "Node.js",
+            "Express.js",
+            "MongoDB",
+            "Mongoose",
+            "Insomnia",
+            "Dotenv",
+        ],
+        "video": "https://youtu.be/ZhuGKlcfDqw",          
+        "image": socialnetwork,
+        "cardClass": "card-column rest",
+        "altText": "Image of the social network backend API calls being made in Insomnia"
+    },
+    {
+        "id": 8,
+        "title": "Code Quiz",
+        "description": "A timed coding quiz with multiple-choice questions. Utilizes vanilla html. css, and javascript to present a series of web-development related questions.",
+        "link": "https://kevinchogan.github.io/code-quiz/",
+        "gitHub": "https://github.com/kevinchogan/code-quiz",
+        "tech": [
+            "HTML/CSS",
+            "Javascript",
+        ],
+        "video": "",          
+        "image": quiz,
+        "cardClass": "card-column rest",
+        "altText": "Image of the code quiz website"
+    },
+    {
+        "id": 9,
+        "title": "Readme Generator",
+        "description": "A node.js application that prompts for project information and automatically generates a high-quality README.md file using that information.",
+        "link": "",
+        "gitHub": "https://github.com/kevinchogan/readme-generator",
+        "tech": [
+            "Node.js",
+            "Inquirer",
+        ],
+        "video": "https://youtu.be/Wr1VyWIsC-I",          
+        "image": readme,
+        "cardClass": "card-column rest",
+        "altText": "Image of the readme generator application running in a VS Code terminal"
+    },
 ]
 return projectList
 }
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -8,16 +8,37 @@ export default function ProjectPage() {
 
   return (
     <section className="project">
-        <div className="card-column">
+      <div className="card-column">
         <figure className="project-card">
-            <h3 className="card-header">{project.title}</h3>
-            <img className="card-image" src={project.image} alt={project.altText} />
-            <div className="card-body">{project.description}</div>
+          <h3 className="card-header">{project.title}</h3>
+          <img
+            className="card-image"
+            src={project.image}
+            alt={project.altText}
+          />
+          <h4>Description</h4>
+          <div className="card-body">{project.description}</div>
+          <h4>Technologies Used</h4>
+          <ul>
+            {project.tech.map((item) => {
+              return <li>{item}</li>;
+            })}
+          </ul>
+          <h4>Links</h4>
+          {project.link && <a href={project.link} target="_blank" className="project-links">
+            Deployed Application
+          </a>}
+          {project.gitHub && <a href={project.gitHub} target="_blank" className="project-links">
+            GitHub Repo
+          </a>}
+          {project.video && <a href={project.video} target="_blank" className="project-links">
+            Video Demonstration
+          </a>}
         </figure>
-        </div>
-        <div className="projectBtn">
-            <Link to="/portfolio">← Go Back</Link>
-        </div>
+      </div>
+      <div className="projectBtn">
+        <Link to="/portfolio">← Go Back</Link>
+      </div>
     </section>
   );
 }
