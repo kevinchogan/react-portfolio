075431e415a12dc70d55654eb2c6f7f055d5b873
Kevin Hogan <kchogan@pacbell.net>
2024-03-02 17:09:09 -0800

    first pass at contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,21 +1,15 @@
 export default function ContactPage() {
     return (
-      <section className="contacts">
+      <form className="contacts">
       <h2 id="Contact_Info">Contact Info</h2>
-      <ul>
-        <li>Phone Number: <a href="tel:9256990145">925-699-0145</a></li>
-        <li>
-          Email: <a href="mailto:kchogan@pacbell.net">kchogan@pacbell.net</a>
-        </li>
-        <li>
-          LinkedIn:
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/kevin-hogan-aa53392/"
-            >Profile</a>
-        </li>
-      </ul>
-    </section>
+      <label for="name">Name:</label>
+      <input type="text" id="name"></input>
+      <label for="email">Email Address:</label>
+      <input type="text" id="email"></input>
+      <label for="message">Message:</label>
+      <textarea type="text" id="message"></textarea>
+      <button if="submit" type="submit">Submit</button>
+    </form>
     );
   }
   
