a5bfbcf3718a8e983161594c630f54cc43099c54
Kevin Hogan <kchogan@pacbell.net>
2024-03-03 15:52:23 -0800

    formatting for nav tabs

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 // Bringing in the required import from 'react-router-dom'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Navbar from './UI/NavBar';
 
 export default function Nav() {
@@ -7,18 +7,18 @@ export default function Nav() {
   return (
     <Navbar
       links={[
-        <Link key={1} className="nav-link text-light" to="/">
+        <NavLink key={1} className="nav-link" activeClassName="active" to="/">
           About Me
-        </Link>,
-        <Link key={2} className="nav-link text-light" to="/portfolio">
+        </NavLink>,
+        <NavLink key={2} className="nav-link" activeClassName="active" to="/portfolio">
           Portfolio
-        </Link>,
-        <Link key={3} className="nav-link text-light" to="/contact">
+        </NavLink>,
+        <NavLink key={3} className="nav-link" activeClassName="active" to="/contact">
           Contact
-        </Link>,
-        <Link key={4} className="nav-link text-light" to="/resume">
+        </NavLink>,
+        <NavLink key={4} className="nav-link" activeClassName="active" to="/resume">
           Resume
-        </Link>,
+        </NavLink>,
       ]}
     />
   );
